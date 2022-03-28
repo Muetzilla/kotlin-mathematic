b23f89faecaebc5e254a77c48119715901f984b7
Muetzilla <muetz-github@gasbichler.ch>
2022-03-28 19:41:06 +0200

    Juhuu Problem 5

--- src/main/kotlin/projecteuler/problem_5/Problem5.kt
+++ src/main/kotlin/projecteuler/problem_5/Problem5.kt
@@ -2,26 +2,25 @@ package projecteuler.problem_5
 
 /**
  * 2520 is the smallest number that can be divided by each of the numbers from 1 to 10 without any remainder.
- * What is the smallest positive number that is evenly divisible by all of the numbers from 1 to 20?
+ * What is the smallest positive number that is evenly diviible by all of the numbers from 1 to 20?
  */
 
-var primeFactors = ArrayList<Int>()
-
 fun main(args: Array<String>) {
-    for (i in 1..20) {
-        getPrimsOfNumber(i)
-    }
-    print("ArrayList of prims: " + primeFactors.toString())
-}
-
-private fun getPrimsOfNumber(i: Int) {
-    var numberToGetPrims = i
-    var tempPrimeFactor = 2
-    while (numberToGetPrims > 1) {
-        while (numberToGetPrims % tempPrimeFactor == 0) {
-            primeFactors.add(tempPrimeFactor)
-            numberToGetPrims /= tempPrimeFactor
+    var currentNumber = 1
+    var smallestValue = 0
+    while (true) {
+        var dividableNoRest = true
+        for (i in 20 downTo 1) {
+            if (currentNumber % i != 0) {
+                dividableNoRest = false
+                break
+            }
         }
-        tempPrimeFactor++;
+        if (dividableNoRest) {
+            smallestValue = currentNumber
+            break
+        }
+        currentNumber++
     }
+    print("Smallest number that can be divided by each of the numbers from 1 to 20 without remainder: " + smallestValue)
 }
